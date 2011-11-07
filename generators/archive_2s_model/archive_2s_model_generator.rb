f899a23e574d09232a7fe5a5452f6bc5023adade
Eric Harrison <eharrison@classifiedadventures.com>
2011-11-07 22:22:10 +1100

    To tiredto do the generator so put it off

--- generators/archive_2s_model/archive_2s_model_generator.rb
+++
@@ -1,7 +0,0 @@
-class Archive2sModelGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.migration_template 'templates/create_archive_2s_model.rb', "db/migrate"
-    end
-  end
-end
