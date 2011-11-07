2e1f9b5b18cc1d73e9d9169cd032e93d07864fcf
Eric Harrison <eharrison@classifiedadventures.com>
2011-11-07 22:15:19 +1100

    bugfix: dropped the wrapper in the generator

--- generators/archive_2s_model/archive_2s_model_generator.rb
+++ generators/archive_2s_model/archive_2s_model_generator.rb
@@ -1,5 +1,7 @@
 class Archive2sModelGenerator < Rails::Generator::Base
   def manifest
-    m.migration_template 'templates/create_archive_2s_model.rb', "db/migrate"
+    record do |m|
+      m.migration_template 'templates/create_archive_2s_model.rb', "db/migrate"
+    end
   end
 end
